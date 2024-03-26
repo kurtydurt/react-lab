4ff9d2f530bd8915af24436cd9aa63e4f36ed62c
Sam Kurtzman <kurtzma4@msu.edu>
2024-03-26 12:30:02 -0400

    creating components, JS logic

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+const now = new Date();
+let muscle;
+const currTime = now.toLocaleDateString();
+if (currTime[0] % 2 == 0) {
+  muscle = 'Chest & Back'
+} else {
+  muscle = 'Leg'
+}
+
+function TodaysLift() {
+
+  return (
+      <>
+        <h2>{muscle} Day : {currTime}</h2>
+      </>
+
+  );
+}
+
+function StartButton() {
+  return (
+      <button>Start Workout</button>
+  );
+}
+
+function Exercises() {
+  const exercises = [
+    {'name' : 'Bench Press','weight' : '225 lbs', 'sets' : '3', 'reps' : '8', 'group': 'Chest & Back'},
+    {'name' : 'Barbell Squat','weight' : '405 lbs', 'sets' : '3', 'reps' : '5', 'group': 'Leg'},
+    {'name' : 'Barbell Bent Over Row','weight' : '135 lbs', 'sets' : '4', 'reps' : '10', 'group': 'Chest & Back'},
+    {'name' : 'Leg Extension','weight' : '100 lbs', 'sets' : '4', 'reps' : '15', 'group': 'Leg'},
+  ]
+  let listExercises = []
+  for ( let exercise of exercises) {
+    if (exercise.group === muscle) {
+      listExercises.push(<li>{exercise.name}: {exercise.weight} - {exercise.sets} x {exercise.reps}</li>);
+    }
+  }
+  return (
+      <ul>{listExercises}</ul>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Damn professor you weren't joking this lab was easy as hell.
-        </a>
+        <TodaysLift/>
+        <Exercises/>
+        <StartButton/>
       </header>
     </div>
   );
